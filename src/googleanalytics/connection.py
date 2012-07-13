5f26f97f6ea1f06e26dbff21d2bf6b64f076ef43
John-Scott Atlakson <john.scott.atlakson@gmail.com>
2012-07-12 20:09:37 -0400

    Raise an exception if profile_id is not found in the connection's accounts.

--- src/googleanalytics/connection.py
+++ src/googleanalytics/connection.py
@@ -39,45 +39,47 @@ class GAConnection:
     def get_accounts(self, start_index=1, max_results=None):
         if not hasattr(self, '_accounts'):
             self._accounts = []
-        base_url = 'https://www.google.com'
-        path = '/analytics/feeds/accounts/default'
-        data = {'start-index': start_index}
-        if max_results:
-            data['max-results'] = max_results
-        data = urllib.urlencode(data)
-        response = self.make_request('GET', base_url, path, data=data)
-        raw_xml = response.read()
-        xml_tree = ElementTree.fromstring(raw_xml)
-        accounts = xml_tree.getiterator('{http://www.w3.org/2005/Atom}entry')
-        for account in accounts:
-            account_data = {
-                'title': account.find('{http://www.w3.org/2005/Atom}title').text,
-                'id': account.find('{http://www.w3.org/2005/Atom}id').text,
-                'updated': account.find('{http://www.w3.org/2005/Atom}updated').text,
-                'table_id': account.find('{http://schemas.google.com/analytics/2009}tableId').text,
-            }
-            for f in account.getiterator('{http://schemas.google.com/analytics/2009}property'):
-                account_data[f.attrib['name']] = f.attrib['value']
-            a = Account(
-                connection=self,
-                title=account_data['title'],
-                id=account_data['id'],
-                updated=account_data['updated'],
-                table_id=account_data['table_id'],
-                account_id=account_data['ga:accountId'],
-                account_name=account_data['ga:accountName'],
-                currency=account_data['ga:currency'],
-                time_zone=account_data['ga:timezone'],
-                profile_id=account_data['ga:profileId'],
-                web_property_id=account_data['ga:webPropertyId'],
-            )
+            base_url = 'https://www.google.com'
+            path = '/analytics/feeds/accounts/default'
+            data = {'start-index': start_index}
+            if max_results:
+                data['max-results'] = max_results
+            data = urllib.urlencode(data)
+            response = self.make_request('GET', base_url, path, data=data)
+            raw_xml = response.read()
+            xml_tree = ElementTree.fromstring(raw_xml)
+            accounts = xml_tree.getiterator('{http://www.w3.org/2005/Atom}entry')
+            for account in accounts:
+                account_data = {
+                    'title': account.find('{http://www.w3.org/2005/Atom}title').text,
+                    'id': account.find('{http://www.w3.org/2005/Atom}id').text,
+                    'updated': account.find('{http://www.w3.org/2005/Atom}updated').text,
+                    'table_id': account.find('{http://schemas.google.com/analytics/2009}tableId').text,
+                }
+                for f in account.getiterator('{http://schemas.google.com/analytics/2009}property'):
+                    account_data[f.attrib['name']] = f.attrib['value']
+                a = Account(
+                    connection=self,
+                    title=account_data['title'],
+                    id=account_data['id'],
+                    updated=account_data['updated'],
+                    table_id=account_data['table_id'],
+                    account_id=account_data['ga:accountId'],
+                    account_name=account_data['ga:accountName'],
+                    currency=account_data['ga:currency'],
+                    time_zone=account_data['ga:timezone'],
+                    profile_id=account_data['ga:profileId'],
+                    web_property_id=account_data['ga:webPropertyId'],
+                )
                 self._accounts.append(a)
         return self._accounts
 
     def get_account(self, profile_id):
+        """Returns an Account object matching the `profile_id` argument."""
         for account in self.get_accounts():
             if account.profile_id == profile_id:
                 return account
+        raise GoogleAnalyticsClientError("%s is not a valid `profile_id`" % profile_id)
 
     def make_request(self, method, base_url, path, headers=None, data=''):
         if headers == None:
